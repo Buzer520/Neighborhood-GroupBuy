c8d789181383b688aef77ab2323e477e40b9899d
EchoLoong <15972042515@163.com>
2026-05-04 01:37:55 +0800

    fix png

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+import { useUserStore } from '@/stores/user'
 
 onLaunch(() => {
   console.log('App Launch')
+  initCloud()
 })
 
 onShow(() => {
@@ -12,11 +14,14 @@ onShow(() => {
 onHide(() => {
   console.log('App Hide')
 })
+
+function initCloud() {
+  const userStore = useUserStore()
+  userStore.loadFromStorage()
+}
 </script>
 
 <style lang="scss">
-@import 'uview-plus/theme.scss';
-
 page {
   background-color: #f5f7fa;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
